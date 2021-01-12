e41f7585515afaa27f257b2c4f3f10105d3fb2a0
Pablo Acevedo Areco <16298855+pabloacvd@users.noreply.github.com>
2021-01-12 19:33:40 -0300

    Update Main.java
    Cambio el archivo desde la nube.

--- src/ar/com/xeven/Main.java
+++ src/ar/com/xeven/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hola mundo");
-
+        String nombre = "Ada";
         System.out.println("Algo más que quiero subir a Github");
     }
 }
