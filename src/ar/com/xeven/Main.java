f6b0ed3bebce010975255893eaa588b56d730828
pabloacvd <pablo@xeven.com.ar>
2021-01-12 19:28:45 -0300

    Agrego una linea adicional a nuestro proyecto.

--- src/ar/com/xeven/Main.java
+++ src/ar/com/xeven/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hola mundo");
+
+        System.out.println("Algo más que quiero subir a Github");
     }
 }
